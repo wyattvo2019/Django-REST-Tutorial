5baba6804295e40dac506f188c82acd15a81682e
Wyatt VO <votranquy96@gmail.com>
2023-11-05 22:48:12 +0700

    Tutorial 2

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -25,5 +25,6 @@ router.register(r'groups', views.GroupViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
+    path('snip/', include('snippets.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
