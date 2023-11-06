fa165a2ad5c2e294d307c1d312f397ac6d973207
Wyatt VO <votranquy96@gmail.com>
2023-11-06 22:34:44 +0700

    Final model

--- snippets/models.py
+++ snippets/models.py
@@ -12,5 +12,19 @@ class Snippet(models.Model):
     linenos = models.BooleanField(default=False)
     # language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
     # style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
+    owner = models.ForeignKey('auth.User', related_name='snippets', on_delete=models.CASCADE)
+    highlighted = models.TextField()
+
+    def save(self, *args, **kwargs):
+        """
+        Use the `pygments` library to create a highlighted HTML
+        representation of the code snippet.
+        """
+        # lexer = get_lexer_by_name(self.language)
+        linenos = 'table' if self.linenos else False
+        options = {'title': self.title} if self.title else {}
+        # formatter = HtmlFormatter(style=self.style, linenos=linenos, full=True, **options)
+        # self.highlighted = highlight(self.code, lexer, formatter)
+        super().save(*args, **kwargs)
     class Meta:
         ordering = ['created']
